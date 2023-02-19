64ff354c8877a0f8b994604e213a4cd9d4107dab
kara4sea <KARASEA@GMAIL.COM>
2023-02-19 15:34:51 -0800

    updated sql

--- billionaires_future_analysis.sql
+++ billionaires_future_analysis.sql
@@ -21,3 +21,17 @@ FROM forbes_2022_billionaires AS tt
 SELECT * FROM finalworth_2018v2022;
 
 DROP TABLE finalworth_2018v2022;
+
+-- list of billionaires with wealth in 2018 v 2022, include all columns
+SELECT  tt.personname,
+        tt.finalWorth_2022,
+        et.finalWorth_2018,
+		tt.selfmade,
+		tt.age,
+		tt.category,
+		tt.country,
+		tt.gender
+INTO finalworth_2018v2022_allcolumns
+FROM forbes_2022_billionaires AS tt
+    INNER JOIN forbes_2018_billionaires AS et
+        ON (et.personname = tt.personname);
